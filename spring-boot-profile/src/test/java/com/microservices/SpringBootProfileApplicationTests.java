8af5f655d2fb7386142945156830be66233cdba7
Kingsley Victor <javaprodigy56@gmail.com>
2021-02-25 20:41:39 +0100

    instantiate wiremock server

--- spring-boot-profile/src/test/java/com/microservices/SpringBootProfileApplicationTests.java
+++ spring-boot-profile/src/test/java/com/microservices/SpringBootProfileApplicationTests.java
@@ -1,11 +1,30 @@
 package com.microservices;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+// import org.aspectj.lang.annotation.After;
+// import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootProfileApplicationTests {
 
+  WireMockServer mockServer = new WireMockServer(5000);
+
+  @BeforeAll
+  void startServer() {
+    this.mockServer.start();
+  }
+
   @Test
-  void contextLoads() {}
+  void runTest() {
+    this.mockServer.stubFor(null);
+  }
+
+  @AfterAll
+  void stopServer() {
+    this.mockServer.stop();
+  }
 }
